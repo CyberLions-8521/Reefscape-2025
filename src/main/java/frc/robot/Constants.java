b70fdea614b6a53a263ef2439f20b3df3434a781
Kyrillos-Hanna <kyrohanna90@gmail.com>
2025-01-09 17:12:34 -0800

    created objects, worked on motor configs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,16 @@ package frc.robot;
 
 public class Constants {
     public static class SwerveConstants {
-        final static int kDriveMotorID = 1; //placeholder
-        final static int kTurnMotorID = 2; //placeholder
+        static final double driveP = 0;
+        static final double driveI = 0;
+        static final double driveD = 0;
+        static final double turnP = 0;
+        static final double turnI = 0;
+        static final double turnD = 0;
+
+        static final int turnMotorFreeLimit = 40; 
+        static final int turnMotorStallLimit = 40;
+        static final int driveMotorFreeLimit = 40;
+        static final int driveMotorStallLimit = 40;
     }
 }
--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -4,19 +4,66 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.proto.Kinematics;
 import frc.robot.Constants.SwerveConstants;
 
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 /** Add your docs here. */
 public class SwerveModule {
-    SparkMax m_driveMotor = new SparkMax(SwerveConstants.kDriveMotorID, SparkLowLevel.MotorType.kBrushless);
-    SparkMax m_turnMotor = new SparkMax(SwerveConstants.kTurnMotorID, SparkLowLevel.MotorType.kBrushless);
-    private SwerveModuleState m_swerveModuleStates = new SwerveModuleState();
+    SparkMax m_driveMotor; 
+    SparkMax m_turnMotor; 
+    private SwerveModuleState m_currentState;
+    private SwerveModuleState m_desiredState;
+    SparkClosedLoopController m_drivePID;
+    SparkClosedLoopController m_turnPID;
+    SparkMaxConfig m_configDrive;
+    SparkMaxConfig m_configTurn;
+    
+
+    public SwerveModule(int driveMotorPort, int turnMotorPort) {
+        m_driveMotor = new SparkMax(driveMotorPort, SparkLowLevel.MotorType.kBrushless);
+        m_turnMotor = new SparkMax(turnMotorPort, SparkLowLevel.MotorType.kBrushless);
+
+        m_drivePID = m_driveMotor.getClosedLoopController();
+        m_turnPID = m_turnMotor.getClosedLoopController();
+
+        m_configDrive = new SparkMaxConfig();
+        m_configTurn = new SparkMaxConfig();
+
+        
+    }
+
+    public void configureMotors() {
+        m_configDrive.idleMode(SparkBaseConfig.IdleMode.kBrake);
+        m_configTurn.idleMode(SparkBaseConfig.IdleMode.kBrake);
+        
+        m_configDrive.inverted(true);
+        m_configTurn.inverted(true);
+
+        m_configDrive.smartCurrentLimit(SwerveConstants.driveMotorStallLimit, SwerveConstants.driveMotorFreeLimit);
+        m_configTurn.smartCurrentLimit(SwerveConstants.turnMotorStallLimit, SwerveConstants.turnMotorFreeLimit);
+
+
+        
+        
+    }
+
+    public SwerveModuleState getSwerveModuleState() {
+        return m_currentState;
+    }
+
+    public void setDesiredSwerveModuleState(SwerveModuleState desiredState) {
+        m_desiredState = desiredState;
+    }
 
 
 }
