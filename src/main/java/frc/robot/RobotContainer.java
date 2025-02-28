bbb77c3ad520b792ff3dff79c1042768954602d0
Kyrillos-Hanna <Kyrohanna90@gmai.com>
2025-02-28 08:57:12 -0800

    autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,9 +37,9 @@ public class RobotContainer {
   private final CommandXboxController m_commandController = new CommandXboxController(OperaterConstants.kCommandControllerPort);
   private final Swerve m_db = new Swerve();
 
-  SlewRateLimiter vx_limiter = new SlewRateLimiter(0.9);
-  SlewRateLimiter vy_limiter = new SlewRateLimiter(0.9);
-  //SlewRateLimiter omega_limiter = new SlewRateLimiter(1.0);
+  SlewRateLimiter vx_limiter = new SlewRateLimiter(SwerveDrivebaseConstants.kSlewRateLimiter);
+  SlewRateLimiter vy_limiter = new SlewRateLimiter(SwerveDrivebaseConstants.kSlewRateLimiter);
+  SlewRateLimiter omega_limiter = new SlewRateLimiter(SwerveDrivebaseConstants.kSlewRateLimiter);
   
   
 
@@ -54,8 +54,8 @@ public class RobotContainer {
  
   private void configureBindings() {
     //m_controller.a().onTrue(new ElevatorGoToSetpoint(.30, m_elevator));
-    m_commandController.rightTrigger().whileTrue(new ElevatorUp(m_elevator, .5));
-    m_commandController.leftTrigger().whileTrue(new ElevatorDown(m_elevator, -.5));
+    m_commandController.rightTrigger().whileTrue(new ElevatorUp(m_elevator, .65));
+    m_commandController.leftTrigger().whileTrue(new ElevatorDown(m_elevator, -.65));
     //m_controller.a().onTrue(m_elevator.resetEncoderCommand());
 
     m_commandController.b().onTrue(new Intake(m_shooter, 14.5)); //intakes
@@ -75,30 +75,33 @@ public class RobotContainer {
     m_driveController.a().onTrue(m_db.resetGyroCommand());
     
     //tune command - right trigger
-    m_driveController.rightTrigger().whileTrue(getTuneDriveCommand (
-            m_driveController::getLeftY, 
-            m_driveController::getLeftX, 
-            m_driveController::getRightX, 
-            m_driveController.getHID()::getRightBumperButton,0.5));
+    // m_driveController.rightTrigger().whileTrue(getDriveCommand (
+    //         1.0,
+    //         m_driveController::getLeftY, 
+    //         m_driveController::getLeftX, 
+    //         m_driveController::getRightX, 
+    //         m_driveController.getHID()::getRightBumperButton));
 
     //regular drive with slew rate applied
     m_db.setDefaultCommand(getDriveCommand (
+            1,
             m_driveController::getLeftY, 
             m_driveController::getLeftX, 
             m_driveController::getRightX, 
-            m_driveController.getHID()::getRightBumperButton,1));
+            m_driveController.getHID()::getRightBumperButton));
     
     //brake driving - left trigger
     m_driveController.leftTrigger().whileTrue(getDriveCommand (
+            0.5,
             m_driveController::getLeftY, 
             m_driveController::getLeftX, 
             m_driveController::getRightX, 
-            m_driveController.getHID()::getRightBumperButton,0.5));
+            m_driveController.getHID()::getRightBumperButton));
    }
 
   public void configureAutos() {
     m_chooser.setDefaultOption("No Auto", null);
-    m_chooser.addOption("Drive Straight", new DriveToDistance(m_db, 3));
+    m_chooser.addOption("Drive Straight", new DriveToDistance(m_db, -4));
   }
 
   public Command getAutonomousCommand() {
@@ -108,42 +111,45 @@ public class RobotContainer {
 
 
     
-  public Command getDriveCommand(Supplier<Double> vx, Supplier<Double> vy, Supplier<Double> omega, Supplier<Boolean> fieldRelative, double multiplier) {
-    double xSpeed = -MathUtil.applyDeadband(vx.get(), ControllerConstants.kDeadband);
-    xSpeed = Math.copySign(xSpeed * xSpeed, xSpeed);
+  public Command getDriveCommand(double multiplier, Supplier<Double> vx, Supplier<Double> vy, Supplier<Double> omega, Supplier<Boolean> fieldRelative) {
+    // double xSpeed = -MathUtil.applyDeadband(vx.get(), ControllerConstants.kDeadband);
+    // xSpeed = vx_limiter.calculate(Math.copySign(xSpeed * xSpeed, xSpeed));
+    // final double xSpeedDelivered = xSpeed * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond;
 
-    double ySpeed = -MathUtil.applyDeadband(vy.get(), ControllerConstants.kDeadband);
-    ySpeed = Math.copySign(ySpeed * ySpeed, ySpeed);
+    // double ySpeed = -MathUtil.applyDeadband(vy.get(), ControllerConstants.kDeadband);
+    // ySpeed = vy_limiter.calculate(Math.copySign(ySpeed * ySpeed, ySpeed));
+    // final double ySpeedDelivered = ySpeed * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond;
 
-    double rOmega = -MathUtil.applyDeadband(omega.get(), ControllerConstants.kDeadband);
-    rOmega = Math.copySign(rOmega * rOmega, rOmega);
+    // double rOmega = -MathUtil.applyDeadband(omega.get(), ControllerConstants.kDeadband);
+    // rOmega = omega_limiter.calculate(Math.copySign(rOmega * rOmega, rOmega));
+    // final double omegaDelivered = (rOmega * multiplier) * SwerveDrivebaseConstants.kMaxAngularSpeed;
     
+    // return new RunCommand(
+    //   () -> m_db.drive(
+    //     (xSpeedDelivered),
+    //     (ySpeedDelivered),
+    //     (omegaDelivered ),
+    //     !fieldRelative.get()),
+    //   m_db); //omega_limiter.calculate(
     return new RunCommand(
-      () -> m_db.drive(
-        (xSpeed * SwerveDrivebaseConstants.kMaxMetersPerSecond),
-        (ySpeed * SwerveDrivebaseConstants.kMaxMetersPerSecond),
-        (rOmega * SwerveDrivebaseConstants.kMaxAngularSpeed),
-        !fieldRelative.get()),
-      m_db); //omega_limiter.calculate(
-    /*
-     * return new RunCommand(
         () -> m_db.drive(
-          -MathUtil.applyDeadband(vx_limiter.calculate(vx.get()) * multiplier, ControllerConstants.kDeadband) * SwerveDrivebaseConstants.kMaxMetersPerSecond,
-          -MathUtil.applyDeadband(vy_limiter.calculate(vy.get()) * multiplier, ControllerConstants.kDeadband) * SwerveDrivebaseConstants.kMaxMetersPerSecond,
-          -MathUtil.applyDeadband(omega.get() * multiplier, ControllerConstants.kDeadband) * SwerveDrivebaseConstants.kMaxAngularSpeed,
+          -vx_limiter.calculate(Math.copySign(MathUtil.applyDeadband(vx.get(),ControllerConstants.kDeadband) * MathUtil.applyDeadband(vx.get(), ControllerConstants.kDeadband), vx.get()))
+            * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond,
+          -vy_limiter.calculate(Math.copySign(MathUtil.applyDeadband(vy.get(),ControllerConstants.kDeadband) * MathUtil.applyDeadband(vy.get(), ControllerConstants.kDeadband), vy.get()))
+            * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond,
+          -omega_limiter.calculate(Math.copySign(MathUtil.applyDeadband(omega.get(),ControllerConstants.kDeadband) * MathUtil.applyDeadband(omega.get(), ControllerConstants.kDeadband), omega.get()))
+            * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond,
           !fieldRelative.get()),
         m_db); //omega_limiter.calculate(
-     * 
-     */
       
   }
 
   public Command getTuneDriveCommand(Supplier<Double> vx, Supplier<Double> vy, Supplier<Double> omega, Supplier<Boolean> fieldRelative, double multiplier) {
     return new RunCommand(
       () -> m_db.drive(
-        -MathUtil.applyDeadband(vx.get() * multiplier, ControllerConstants.kDeadband) * SwerveDrivebaseConstants.kMaxMetersPerSecond,
-        -MathUtil.applyDeadband(vy.get() * multiplier, ControllerConstants.kDeadband) * SwerveDrivebaseConstants.kMaxMetersPerSecond,
-        -MathUtil.applyDeadband(omega.get() * multiplier, ControllerConstants.kDeadband) * SwerveDrivebaseConstants.kMaxAngularSpeed,
+        -MathUtil.applyDeadband(vx.get() , ControllerConstants.kDeadband) * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond,
+        -MathUtil.applyDeadband(vy.get() , ControllerConstants.kDeadband) * multiplier * SwerveDrivebaseConstants.kMaxMetersPerSecond,
+        -MathUtil.applyDeadband(omega.get() , ControllerConstants.kDeadband) * multiplier * SwerveDrivebaseConstants.kMaxAngularSpeed,
         !fieldRelative.get()),
       m_db); //omega_limiter.calculate(
   }
